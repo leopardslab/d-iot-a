224131bbde882581caa364e8e5c158e0ee5bf3ba
vagrant <vagrant@vagrant.vm>
2020-06-25 13:15:05 +0000

    Access Control Chain Code

---
+++ chaincodes/src/access_contract.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	sc "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/hyperledger/fabric/common/flogging"
+
+	"github.com/hyperledger/fabric-chaincode-go/pkg/cid"
+)
+
+
+// Init ;  Method for initializing smart contract
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil)
+}
